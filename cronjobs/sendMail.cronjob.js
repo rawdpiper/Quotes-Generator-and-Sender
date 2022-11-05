4b7bf288f70f5a675c7db91dce62896458296adc
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-11-05 20:27:14 +0100

    testing

--- cronjobs/sendMail.cronjob.js
+++ cronjobs/sendMail.cronjob.js
@@ -2,7 +2,7 @@ const cron = require('node-cron');
 const sendEmail = require('../utils/sendMail');
 
 cron.schedule(
-  "21 20 * * *",
+  "* 20 * * *",
   async () => {
     console.log("running cron job");
     await sendEmail();
