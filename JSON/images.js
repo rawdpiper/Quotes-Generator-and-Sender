4ff22ba6e926c21f1d08783484c64b69c3fe075a
Hamzat Azeez <azeezhamzy99@gmail.com>
2022-12-03 21:20:40 +0100

    Updated Image URLs

--- JSON/images.js
+++ JSON/images.js
@@ -8,4 +8,6 @@ module.exports = [
   "https://res.cloudinary.com/rawdpiper/image/upload/v1669827127/Koche/photo1669826571_6_il3c73.jpg",
   "https://res.cloudinary.com/rawdpiper/image/upload/v1669827127/Koche/photo1669826571_1_oxpemc.jpg",
   "https://res.cloudinary.com/rawdpiper/image/upload/v1669827183/Koche/photo1669826571_5_ragnkj.jpg",
+  "https://res.cloudinary.com/rawdpiper/image/upload/v1670098604/Koche/photo1670098340_xmpqa4.jpg",
+  "https://res.cloudinary.com/rawdpiper/image/upload/v1670098605/Koche/photo1670098340_1_uawcyt.jpg",
 ];
